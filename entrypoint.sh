3209ed5662f192684b160d686e8b79aabeaac634
flyin <alex@alex-web.ru>
2016-11-26 17:04:04 +0300

    Set one second pause between connect

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-while ! nc -w 1 -z ${DB_HOST} ${DB_PORT}; do sleep 0.1; done
+while ! nc -w 1 -z ${DB_HOST} ${DB_PORT}; do sleep 1; done
 
 python manage.py migrate
 python manage.py collectstatic --noinput
